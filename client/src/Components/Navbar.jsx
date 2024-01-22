29322fe553cbdd85e36a9e5f9cb1459c9bc3f3b1
SachidanandaTripathy <sachidananda.tripathy03@gmail.com>
2024-01-22 19:50:06 +0530

    few changes

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Styles/Navbar.css';
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg custom-navbar">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">DabbaWala</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Orders</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Dabbawalas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">DeliveryTracking</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">DabbawalaCommunity</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" >
+                        <button>
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
